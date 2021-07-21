571a4fad0eb4dc6895a4e9c24672360f77e7a3df
OnSeMiRo <79685152+OnSe-MiRo@users.noreply.github.com>
2021-07-21 16:56:11 +0900

    Update Merge_Pointcloud2.py

--- Merge_Pointcloud2.py
+++ Merge_Pointcloud2.py
@@ -30,7 +30,7 @@ class Merge_Pointcloud2:
 
     def recallback2(self,data):
         pc1=point_cloud2.read_points(data,skip_nans=True, field_names=('x','y','z','intensity'))
-        points2=[]
+        points=[]
         for p in pc1:
             x=float(p[0])
             y=float(p[1])
@@ -38,15 +38,15 @@ class Merge_Pointcloud2:
             i=float(p[3])
             pt1=[x,y,z,i]
             points2.append(pt1)
-        points2=points2+self.point1
-        fields1 = [PointField('x', 0, PointField.FLOAT32, 1),
+        points=points+self.point1
+        fields = [PointField('x', 0, PointField.FLOAT32, 1),
         PointField('y', 4, PointField.FLOAT32, 1),
         PointField('z', 8, PointField.FLOAT32, 1),
         PointField('intensity', 12, PointField.UINT32, 1),
         ]
-        header1 = Header()
-        header1.frame_id = "map"
-        pc21 = point_cloud2.create_cloud(header1, fields1, points2)
+        header = Header()
+        header.frame_id = "map"
+        pc21 = point_cloud2.create_cloud(header, fields, points)
         self.pub1.publish(pc21)
 
 if __name__=='__main__':
